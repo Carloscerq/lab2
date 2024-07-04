c98cd22d84a17af1fab202da285352f2072bc76d
Carlos Cerqueira <ce.cesc01@gmail.com>
2024-07-04 18:07:04 -0300

    feat: add display

--- main.c
+++ main.c
@@ -9,7 +9,70 @@
 #pragma config FPLLMUL = MUL_20, FPLLIDIV = DIV_2, FPLLODIV = DIV_1, FWDTEN = OFF
 #pragma config POSCMOD = HS, FNOSC = PRIPLL, FPBDIV = DIV_8
 
-main()
+#define SYS_FREQ        (80000000L)  // System frequency
+#define SPI_BAUD_RATE   1000000      // Desired SPI Baud Rate
+
+#define TFT_CS     LATBbits.LATB2 // Chip Select pin
+#define TFT_DC     LATBbits.LATB3 // Data/Command pin
+#define TFT_RST    LATBbits.LATB4 // Reset pin
+
+// Delay function using Timer1
+void delay_ms(unsigned int ms) {
+    while (ms--) {
+        WriteTimer1(0); // Reset Timer1 count
+        while (ReadTimer1() < (SYS_FREQ / 256 / 1000)); // Wait for 1 ms
+    }
+}
+
+void SPI_Init() {
+    unsigned int pbClk;
+    pbClk = SYSTEMConfigPerformance(SYS_FREQ);
+    unsigned int spiClk = pbClk / 2; // SPI Clock
+
+    // Configure SPI2 settings
+    SpiChnOpen(SPI_CHANNEL2,
+               SPI_OPEN_MSTEN |       // Master mode
+               SPI_OPEN_CKE_REV |     // Clock edge select
+               SPI_OPEN_MODE8 |       // 8-bit mode
+               SPI_OPEN_ON,           // Turn on SPI
+               spiClk / SPI_BAUD_RATE); // Set SPI clock
+}
+
+void ILI9341_Init(void) {
+    // Reset the display
+    TFT_RST = 0;
+    delay_ms(100);
+    TFT_RST = 1;
+    delay_ms(100);
+
+    // Chip select low to start communication
+    TFT_CS = 0;
+
+    // Initialization sequence
+    ILI9341_WriteCommand(0xEF);
+    ILI9341_WriteData(0x03);
+    ILI9341_WriteData(0x80);
+    ILI9341_WriteData(0x02);
+
+    // Add more initialization commands as needed...
+
+    // Chip select high to end communication
+    TFT_CS = 1;
+}
+
+void ILI9341_WriteCommand(uint8_t cmd) {
+    TFT_DC = 0; // Command mode
+    SpiChnPutC(SPI_CHANNEL2, cmd);
+    while (SpiChnIsBusy(SPI_CHANNEL2));
+}
+
+void ILI9341_WriteData(uint8_t data) {
+    TFT_DC = 1; // Data mode
+    SpiChnPutC(SPI_CHANNEL2, data);
+    while (SpiChnIsBusy(SPI_CHANNEL2));
+}
+
+int main()
 {
     DDPCONbits.JTAGEN = 0;
 
@@ -25,6 +88,12 @@ main()
     TRISAbits.TRISA3 = 0;
     TRISAbits.TRISA4 = 0;
     TRISAbits.TRISA5 = 0;
+    
+    // Set Timer1 prescaler to 1:256
+    OpenTimer1(T1_ON | T1_SOURCE_INT | T1_PS_1_256, 0xFFFF);
+
+    // Set Timer1 interrupt priority to lowest (optional)
+    ConfigIntTimer1(T1_INT_ON | T1_INT_PRIOR_1);
     
     struct globalStatus applicationStatus;
     applicationStatus.ledState[0] = IS_OFF;
--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -101,17 +101,17 @@ endif
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/f9139d6d52bd2871025e31369406f8bc0d4b698d .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/cc3e382b7d517e33d95ea33197e08c557a9c83ec .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/eventHandler.o: eventHandler.c  .generated_files/flags/default/945e356a9a368c169163a282aa17a17daad27f88 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/eventHandler.o: eventHandler.c  .generated_files/flags/default/ea35b587da2432d5a8981e8293b4bd2f83950f5d .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/eventHandler.o.d 
 	@${RM} ${OBJECTDIR}/eventHandler.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/eventHandler.o.d" -o ${OBJECTDIR}/eventHandler.o eventHandler.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/eventHandler.o.d" -o ${OBJECTDIR}/eventHandler.o eventHandler.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
 else
 ${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/9aec86419e3441c17328bce5c696ddbe2b83deb4 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
@@ -139,7 +139,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 ${DISTDIR}/Trabalho_Lab_2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  ../../../../Program\ Files\ (x86)/microchip/xc32/v1.40/pic32mx/include/plib.h  
 	@${MKDIR} ${DISTDIR} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_SIMULATOR=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o ${DISTDIR}/Trabalho_Lab_2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}    "..\..\..\..\Program Files (x86)\microchip\xc32\v1.40\pic32mx\include\plib.h"      -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)      -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,--defsym=_min_heap_size=512,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,${DISTDIR}/memoryfile.xml -mdfp="${DFP_DIR}"
+	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_PK3=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o ${DISTDIR}/Trabalho_Lab_2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}    "..\..\..\..\Program Files (x86)\microchip\xc32\v1.40\pic32mx\include\plib.h"      -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)   -mreserve=data@0x0:0x1FC -mreserve=boot@0x1FC02000:0x1FC02FEF -mreserve=boot@0x1FC02000:0x1FC024FF  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_PK3=1,--defsym=_min_heap_size=512,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,${DISTDIR}/memoryfile.xml -mdfp="${DFP_DIR}"
 	
 else
 ${DISTDIR}/Trabalho_Lab_2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  ../../../../Program\ Files\ (x86)/microchip/xc32/v1.40/pic32mx/include/plib.h 
